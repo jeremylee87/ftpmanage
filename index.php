b3ac205c484ba43cc6ecc26ed2b1c91c656dc140
jeremylee87 <shanshi1987@163.com>
2016-07-30 12:47:35 +0800

    add add data style

--- index.php
+++ index.php
@@ -4,79 +4,87 @@
 	<meta charset="UTF-8">
 	<title>站点信息</title>
 	<style>
+		*{font-family: "Micosoft yahei"}
 		h2{text-align: center;}
+		table{border:1px solid #999;}
 		table.t_talbe{
 			font-size:11px;
-			color:#333333;
-			border-width: 1px;
-			border-color: #666666;
+			border-collapse:collapse;border:none;
+
 		}
 		table.t_table th{
-			border-width: 1px;
-			padding: 2px;
-			border-style: solid;
-			border-color: #666666;
+			border:#999 solid 1px;
 			background-color: #dedede;
 			font-size: 12px;
 			line-height: 1.2em;
 		}
 		table.t_table td{
-			border-width: 1px;
-			padding: 2px;
-			border-style: solid;
-			border-color: #666666;
-			background-color: #ffffff;
+			border:#999 solid 1px;
+			text-align: center;
 			font-size: 12px;
 			line-height: 1.2em;
 		}
-		input{font-size: 9px;}
+		input,select{margin:0;padding:0;}
+		input[type=text]{font-size: 9px;height: 1.8em;line-height: 1.8em;}
+		input:focus{
+    			border-style:solid;
+    			border-color:blue;
+			}
 		.add_rs{margin:10px;display: block;width:150px;text-align: center;font-weight: bold;color: red;}
-		.bt_name{}
+		.bt_name{height: 2em;}
 		.bt_name td{font-size: 12px;font-weight: bold;}
-		.td_modi{font-size: 14px;}
+		.td_modi a{font-size: 1.2em;}
+		.site_name{width: 80px;}
+		.site_type{width: 65px;}
+		.ftp_name{width: 90px;}
+		.ftp_pwd{width: 120px;}
+		.ftp_url{width: 70px;}
+		.db_name,.db_pwd,.modi_time{width: 75px;}
+		.db_user,.login_name{width: 60px;}
+		.login_url{width: 150px;}
+		.login_pwd{width: 75px;}
+		.add_tr{height: 2em;}
+		.add_ok{font-size: 1.5em;}
 	</style>
+	<script language="javascript">
+		function submitcheck(obj) {
+				if (document.getElementById('site_name').value == '') {
+            		alert('必须输入站点名称！');
+           			document.getElementById('site_name').focus();
+            		return false;
+        		} else if (document.getElementById('ftp_url').value == '') {
+            		alert('必须输入Ftp地址！');
+            		document.getElementById('ftp_url').focus();
+           			return false;
+        		}else if(document.getElementById('ftp_name').value ==''){
+        			alert('必须输入Ftp账号！');
+            		document.getElementById('ftp_name').focus();
+           			return false;
+        		}else if(document.getElementById('ftp_pwd').value ==''){
+        			alert('必须输入Ftp密码！');
+            		document.getElementById('ftp_pwd').focus();
+           			return false;
+        		}
+        		 else {
+            		obj.submit();
+        		}
+			}
+			function freset(obj){
+				obj.reset();
+			}
+</script>
 </head>
 <body>
 <h2>FTP信息管理系统</h2>
-<a class="add_rs" href="add.php">添加记录</a>
+<!--<a class="add_rs" href="add.php">添加记录</a>-->
 	<table class="t_table" border="0" cellpadding="0" cellspacing="0"  id="ftpinfo">
-		<!--添加表格
-		 
-<form action="addok.php" method="post">
-			 <input type="hidden" name="id" value=""/>
-
-			 <tr>
-			 	<td><input type="reset" name="reset" value="重置"><input type="submit" name="submit" value="添加"></td>
-				<td><input type="text" name="site_name" required="required" ></td>
-				<td>
-				<select name="site_type">
-					<option value="PC">PC</option>
-					<option value="Mobile">Mobile</option>
-				</select>
-				</td>
-				<td><input type="text" name="ftp_url"></td>
-				<td><input type="text" name="ftp_name"></td>
-				<td><input type="text" name="ftp_pwd"></td>
-				<td><input type="text" name="db_name"></td>
-				<td><input type="text" name="db_user"></td>
-				<td><input type="text" name="db_pwd"></td>
-				<td><input type="text" name="login_url"></td>
-				<td><input type="text" name="login_name"></td>
-				<td><input type="text" name="login_pwd"></td>
-				<td><input type="text" name="tips"></td>
-				<td></td>
-			</tr>
-
-			</form>
-添加表格结束-->
-
 			<tr class="bt_name">
 				<th>操作</th>
-				<th>站点名称(必填)</th>
+				<th>站点名称(*)</th>
 				<th>类型</th>
-				<th>FTP地址</th>
-				<th>FTP账号</th>
-				<th>FTP密码</th>
+				<th>FTP地址(*)</th>
+				<th>FTP账号(*)</th>
+				<th>FTP密码(*)</th>
 				<th>数据库名称</th>
 				<th>数据库用户</th>
 				<th>数据库密码</th>
@@ -86,7 +94,34 @@
 				<th>备注</th>
 				<th>修改时间</th>
 			</tr>
-
+			<!--添加表格-->
+			 <tr>
+			 <form id="add_rs" name="add_rs" action="addok.php" method="post">
+			 <input type="hidden" name="id" value=""/>
+			 <td class="add_tr"><a class="add_ok" href="javascript:freset(document.add_rs);" >重置</a></td>
+			 	<!--<td><input type="reset" name="reset" value="重置"><input type="submit" name="submit" value="添加"></td>-->
+				<td><input class="site_name" type="text" name="site_name" id="site_name" required="required" ></td>
+				<td>
+				<select class="site_type" name="site_type">
+					<option value="PC">PC</option>
+					<option value="Mobile">Mobile</option>
+				</select>
+				</td>
+				<td><input class="ftp_url" type="text" name="ftp_url" id="ftp_url" required="required"></td>
+				<td><input class="ftp_name" type="text" name="ftp_name" id="ftp_name" required="required"></td>
+				<td><input class="ftp_pwd" type="text" name="ftp_pwd" id="ftp_pwd" required="required"></td>
+				<td><input class="db_name" type="text" name="db_name"></td>
+				<td><input class="db_user" type="text" name="db_user"></td>
+				<td><input class="db_pwd" type="text" name="db_pwd"></td>
+				<td><input class="login_url" type="text" name="login_url"></td>
+				<td><input class="login_name" type="text" name="login_name"></td>
+				<td><input class="login_pwd" type="text" name="login_pwd"></td>
+				<td><input class="tips " type="text" name="tips"></td>
+				<td class="modi_time"><a class="add_ok" href="javascript:void(0)" onclick="submitcheck(document.add_rs)">添加</a></td>
+				</form>
+			</tr>
+			<!--添加表格结束-->
+		<!--数据循环开始-->
 			<?php 
 				require_once 'conn.php';  //引入数据库链接
 				date_default_timezone_set("PRC"); //设置时区
@@ -105,7 +140,7 @@
 
 					<tr >
 						<td width=60 class='td_modi'>
-						<a href='add.php'>增</a>
+						<!--<a href='add.php'>增</a>-->
 						<a href='edit.php?id=".$ftpcon['id']."'> 改</a>
 						<a href='del.php?id=".$ftpcon['id']."' onclick='return confirm(\"是否真的要删除当前记录?\")'> 删</a>
 
@@ -130,12 +165,37 @@
 					";
 				}
 
-    
-
 			 ?>
 
+<!--数据循环结束-->
 
-
+<!--添加表格-->
+			 <tr>
+			 <form id="add_rs" name="add_rs" action="addok.php" method="post">
+			 <input type="hidden" name="id" value=""/>
+			 <td class="add_tr"><a class="add_ok" href="javascript:freset(document.add_rs);" >重置</a></td>
+			 	<!--<td><input type="reset" name="reset" value="重置"><input type="submit" name="submit" value="添加"></td>-->
+				<td><input class="site_name" type="text" name="site_name" id="site_name" required="required" ></td>
+				<td>
+				<select class="site_type" name="site_type">
+					<option value="PC">PC</option>
+					<option value="Mobile">Mobile</option>
+				</select>
+				</td>
+				<td><input class="ftp_url" type="text" name="ftp_url" id="ftp_url" required="required"></td>
+				<td><input class="ftp_name" type="text" name="ftp_name" id="ftp_name" required="required"></td>
+				<td><input class="ftp_pwd" type="text" name="ftp_pwd" id="ftp_pwd" required="required"></td>
+				<td><input class="db_name" type="text" name="db_name"></td>
+				<td><input class="db_user" type="text" name="db_user"></td>
+				<td><input class="db_pwd" type="text" name="db_pwd"></td>
+				<td><input class="login_url" type="text" name="login_url"></td>
+				<td><input class="login_name" type="text" name="login_name"></td>
+				<td><input class="login_pwd" type="text" name="login_pwd"></td>
+				<td><input class="tips " type="text" name="tips"></td>
+				<td class="modi_time"><a class="add_ok" href="javascript:void(0)" onclick="submitcheck(document.add_rs)">添加</a></td>
+				</form>
+			</tr>
+			<!--添加表格结束-->
 		
 		</table>
 
